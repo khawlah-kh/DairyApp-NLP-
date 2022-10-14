ae9569d0ec234c3d52f164ef5652b39b2ce0c297
Khawlah Khalid <khawlah1920@hotmail.com>
2022-10-14 07:47:43 +0300

    Initial Commit

---
+++ NLPDairy/NLPDairyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NLPDairyApp.swift
+//  NLPDairy
+//
+//  Created by Khawlah Khalid on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct NLPDairyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
